18f37a7d7f2230c136faa8cdf578d2bea4f4b31d
DF <dennisforen@gmail.com>
2025-01-25 17:43:00 -0500

    replace manual timer for informant event with async timer

--- new-game-project/root.gd
+++ new-game-project/root.gd
@@ -1,31 +1,33 @@
 extends Node
+
+@export var timeBetweenInformantEventsInSeconds = 1.0;
+
 var stocks = []
-@export var timeToNextEvent = 0;
-var deltaTimer = 0.0
-var delayTimer = 0.0
+
+var isInformantEventsOn = false:
+	set(value):
+		if (value):
+			start_informant_event_timer();
+		isInformantEventsOn = value;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	stocks = $Stocks.get_children()
-	setTimer()
-
+	isInformantEventsOn = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-	
-	deltaTimer += delta
-	if deltaTimer >= timeToNextEvent:
-		delayTimer += delta
-		if delayTimer >= 10.0:
-			informant_event()
-			deltaTimer = 0
-			delayTimer = 0
-			setTimer()
 
+## Informant Events
+func start_informant_event_timer() -> void:
+	await get_tree().create_timer(timeBetweenInformantEventsInSeconds).timeout
+	if (isInformantEventsOn):
+		trigger_informant_event()
+		start_informant_event_timer()
 
-func informant_event() -> void:
+func trigger_informant_event() -> void:
 	var eventType = randi() % 9
 	if eventType == 8:
 		print("No Event")
@@ -38,16 +40,13 @@ func informant_event() -> void:
 			print("Negative " + stockName + " informant event")
 		$Informant.trigger(stockName, positive)
 		stocks[eventType].eventTrigger(positive)
-		
-func setTimer() -> void:
-	timeToNextEvent = randi() % 11 + 0
-		
+
 func getTVTrend(channel) -> bool:
 	var stock = stocks[channel]
 	return true if stock.pastPrice < stock.currPrice else false
 
 func getPastSeven(channel) -> Array:
 	return stocks[channel].pastSeven
-	
+
 func getRadioPrediction(channel) -> bool:
 	return stocks[channel].getPrediction()
