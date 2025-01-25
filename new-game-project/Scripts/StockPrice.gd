4f153dc989584330e03bd6d694e4a6cdc32a4894
Dan Squire <dss9593@rit.edu>
2025-01-25 09:33:13 -0500

    Initial version of stock pop system

--- new-game-project/Scripts/StockPrice.gd
+++ new-game-project/Scripts/StockPrice.gd
@@ -2,30 +2,39 @@ extends Node
 
 @export var currPrice = 0
 @export var rateChangeIn = 0
+var bubbleMin = 500
+var bubbleMax = 750
 var rate = 0
 var maxRate = 50
 var minRate = 0
 var deltCountPriceChange = 0.0
 var deltCountRateChange = 0.0
+var popped = false
+var popGoal = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setRate()
+	setRateChange()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	deltCountPriceChange += delta
-	deltCountRateChange += delta
 	if deltCountPriceChange >= 1:
 		deltCountPriceChange -= 1
 		update()
-	if deltCountRateChange >= rateChangeIn:
-		deltCountRateChange -= rateChangeIn
-		setRate()
-		setRateChange()
+		
+	if !popped:
+		deltCountRateChange += delta
+		if deltCountRateChange >= rateChangeIn:
+			deltCountRateChange -= rateChangeIn
+			setRate()
+			setRateChange()
 
 func update() -> void:
 	currPrice += rate
+	if currPrice < 0:
+		currPrice = 0
 	print(currPrice)
 	
 func setRate() -> void:
@@ -36,3 +45,11 @@ func setRate() -> void:
 
 func setRateChange() -> void:
 	rateChangeIn = randi() % 31 + 10
+	
+func setBubble():
+	pass
+
+func pop():
+	popped = true
+	popGoal = currPrice / 10
+	rate = abs(rate) * -3
--- new-game-project/root.gd
+++ new-game-project/root.gd
@@ -10,4 +10,3 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-	pass
