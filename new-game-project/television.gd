53f38dbe54806f3e2f0671fa8db14f5a26b905b0
Ryce Hazard (RIT Student) <rrh3969@rit.edu>
2025-01-25 12:25:11 -0500

    television modes

---
+++ new-game-project/television.gd
@@ -0,0 +1,26 @@
+extends AnimatedSprite2D
+
+var modes = 3
+var mode = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	match mode:
+		1:
+			print("mode 1")
+		2:
+			print("mode 2")
+		3:
+			print("mode 3")
+
+
+func _on_sprite_2d_object_clicked():
+	if(mode >= modes):
+		mode = 1
+	else:
+		mode += 1
