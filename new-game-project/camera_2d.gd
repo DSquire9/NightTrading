84c87d095d39735ed774fdeb56036a2a66ea8b26
Ryce Hazard (RIT Student) <rrh3969@rit.edu>
2025-01-24 21:44:10 -0500

    attempt to lerp camera

--- new-game-project/camera_2d.gd
+++ new-game-project/camera_2d.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 var screens = [0, 2000]
 var currentScreen = 0
+var t = 0.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,12 +12,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(Input.is_action_just_released("move camera left")):
+	t += delta * 0.4
+	if(Input.is_action_just_released("move camera left") && !(currentScreen < 1)):
 		currentScreen = currentScreen - 1
-		if(currentScreen < 0):
-			currentScreen = screens.size() - 1
-	if(Input.is_action_just_released("move camera right")):
+		#if(currentScreen < 0):
+			#currentScreen = screens.size() - 1
+	if(Input.is_action_just_released("move camera right") && !(currentScreen >= screens.size() - 1)):
 		currentScreen = currentScreen + 1
-		if(currentScreen >= screens.size()):
-			currentScreen = 0
+		#if(currentScreen >= screens.size()):
+			#currentScreen = 0
+	#offset = offset.lerp(Vector2(screens[currentScreen],0), t)
+	t = 0.0
 	offset.x = screens[currentScreen]
