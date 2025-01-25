a15d83fa11651869a220af0c61b2487abf60b77e
Ryce Hazard (RIT Student) <rrh3969@rit.edu>
2025-01-25 13:59:35 -0500

    menu navagation

---
+++ new-game-project/main.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	get_node("MainTitle").visible = false
+	get_node("Game").visible = true
+
+
+func _on_end_button_pressed():
+	get_node("EndScreen").visible = false
+	get_node("MainTitle").visible = true
+
+
+func _on_game_button_pressed():
+	get_node("Game").visible = false
+	get_node("Pause").visible = true 
+
+
+func _on_pause_button_pressed():
+	get_node("Pause").visible = false
+	get_node("EndScreen").visible = true
+
+
+func _on_pause_button_2_pressed():
+	get_node("Pause").visible = false
+	get_node("Game").visible = true
