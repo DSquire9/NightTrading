9d369b466b2c09c4bafd79ec7b232df986694dfc
Noah O'Neill <noahnogg@gmail.com>
2025-01-25 16:06:23 -0500

    I created a signal that the timer emits when it reaches zero and the font was changed to one of the custom fonts

--- new-game-project/label.gd
+++ new-game-project/label.gd
@@ -4,6 +4,8 @@ var sec1 = 0
 var sec10 = 0
 var minutes = 5
 
+signal countdownEnd
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text = str(minutes) + ":" + str(sec10) + str(sec1)
@@ -26,4 +28,7 @@ func clock_counting() -> void:
 	if sec10 < 0:
 		sec10 = 5
 		minutes -= 1
+		
+	if minutes == 0:
+		countdownEnd.emit()
 	pass # Replace with function body.
--- new-game-project/timer.gd
+++ new-game-project/timer.gd
@@ -1,6 +1,5 @@
 extends Timer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -21,4 +20,9 @@ func pause_or_start() -> void:
 
 func reset() -> void:
 	wait_time = 1
+	pass # Replace with function body.
+	
+
+func _on_label_countdown_end() -> void:
+	stop()
 	pass # Replace with function body.
