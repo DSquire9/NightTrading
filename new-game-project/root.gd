986846daa3d67767af9455b82cbc4f06ade8ce8e
Dan Squire <dss9593@rit.edu>
2025-01-25 08:48:37 -0500

    Update root.gd

--- new-game-project/root.gd
+++ new-game-project/root.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("end"):
+	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
 	pass
