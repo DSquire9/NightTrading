fb6d2588a95945661ba6b08a9ab52eaf4656fd38
Ryce Hazard (RIT Student) <rrh3969@rit.edu>
2025-01-25 12:00:13 -0500

    click object with event

--- new-game-project/click_object.gd
+++ new-game-project/click_object.gd
@@ -1,7 +1,8 @@
 extends Sprite2D
 
+signal object_clicked()
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
-			#place event call here
-			print("object clicked")
+			object_clicked.emit()
