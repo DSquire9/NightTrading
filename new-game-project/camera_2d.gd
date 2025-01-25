86b744da586e43d57ec519ea85863bb7d9e2309e
Ryce Hazard (RIT Student) <rrh3969@rit.edu>
2025-01-25 10:05:09 -0500

    camera working better

--- new-game-project/camera_2d.gd
+++ new-game-project/camera_2d.gd
@@ -1,9 +1,10 @@
 extends Camera2D
 
-var screens = [0, 2000, 4000]
+var screens = [0, 1500, 3000]
 var currentScreen = 0
 var t = 0.0
 var start = Vector2(0,0)
+var moving = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,19 +13,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(t < 1.0):
+	if(t < 1.0 && moving):
 		t += delta * 0.4
 	
 	if(Input.is_action_just_released("move camera left") && !(currentScreen < 1)):
 		currentScreen = currentScreen - 1
 		t = 0.0
 		start = offset
+		moving = true
 	if(Input.is_action_just_released("move camera right") && !(currentScreen >= screens.size() - 1)):
 		currentScreen = currentScreen + 1
 		t = 0.0
 		start = offset
+		moving = true
 	
 	offset = start.lerp(Vector2(screens[currentScreen],0), t)
 	if(t >= 1.0):
 		t = 0.0
 		start = offset
+		moving = false
